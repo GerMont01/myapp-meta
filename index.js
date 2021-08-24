302c15d081c26c7c116dbb81f4835c318783e4e2
Gerardo Montero <gerardomontme@gmail.com>
2021-08-24 15:10:27 -0700

    login with basic auth

--- index.js
+++ index.js
@@ -1,48 +1,42 @@
 const express = require('express');
 const basicAuth = require('express-basic-auth');
 const cors = require('cors');
-const session = require('express-session');
-const cookieParser = require('cookie-parser');
+// const session = require('express-session');
+// const cookieParser = require('cookie-parser');
 
 
 const app = express();
-
-app.use(cors({ 
-    origin: 'http://localhost:3000',
-    credentials: true
-}));
-app.use(cookieParser());
-app.use(session({
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false },
-    secret: '1232abbc1a23123f123e123a'
-}));
+app.use(cors())
+// app.use(cors({ 
+//     origin: 'http://localhost:3000',
+//     credentials: true
+// }));
+// app.use(cookieParser());
+// app.use(session({
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: { secure: false },
+//     secret: '1232abbc1a23123f123e123a'
+// }));
 
 app.use(express.json());
 
-let Users = [{id: 'admin', password: 'supersecret'}];
-// app.use(basicAuth({
-//     authorizer: (username, password) => {
-//         const userMatches = basicAuth.safeCompare(username, 'admin')
-//         const passwordMatches = basicAuth.safeCompare(password, 'supersecret')
-//         return userMatches & passwordMatches
-//     },
-//     unauthorizedResponse: (req) => {
-//         return `unauthorized. ip: ${req.ip}`
-//     }
-// }))
+// let Users = [{id: 'admin', password: 'supersecret'}];
+app.use(basicAuth({
+    authorizer: (username, password) => {
+        const userMatches = basicAuth.safeCompare(username, 'admin')
+        const passwordMatches = basicAuth.safeCompare(password, 'supersecret')
+        return userMatches & passwordMatches
+    },
+    unauthorizedResponse: (req) => {
+        return `unauthorized. ip: ${req.ip}`
+    }
+}))
 
-app.post('/login', (req,res,next) => {
-    Users.filter(user => {
-        const userMatches = basicAuth.safeCompare(req.body.username, user.id)
-        const passwordMatches = basicAuth.safeCompare(req.body.password, user.password)
-        if (userMatches && passwordMatches){
-            req.session.user = user;
-            console.log(req.session)
-            res.json('logged in')
-        }
-    })
+app.get('/login', (req,res,next) => {
+            
+    res.json('logged in')
+
 })
 
 
